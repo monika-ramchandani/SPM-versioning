1c1ca7f621c91ffe7ce3496ab42c7ea208994457
Monika Ramchandani <monika.ramchandani@iongroup.com>
2023-03-27 16:00:01 +0530

    Initial commit

---
+++ Sources/SPM-versioning/SPM_versioning.swift
@@ -0,0 +1,6 @@
+public struct SPM_versioning {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SPM-versioningTests/SPM_versioningTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SPM_versioning
+
+final class SPM_versioningTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SPM_versioning().text, "Hello, World!")
+    }
+}
